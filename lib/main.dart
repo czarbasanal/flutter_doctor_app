182d89f88df55d69caeb9e27b26a1c9127ab375e
MexlTuba <mexldelvertuba@gmail.com>
2023-10-17 14:09:03 +0800

    deleted files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_doctor_app/screens/doctor_details.dart';
-import 'screens/home_page.dart';
 import 'models/doctor.dart';
 
 void main() {
