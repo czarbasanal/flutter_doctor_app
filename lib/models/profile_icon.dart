ea83bd7e986cdee53633ab6ece97b0024a9c6593
Czar Basanal <czarbasanal@gmail.com>
2023-10-21 21:40:45 +0800

    final commit

--- lib/models/profile_icon.dart
+++ lib/models/profile_icon.dart
@@ -12,43 +12,24 @@ class ProfileIcon extends StatelessWidget {
         alignment: const Alignment(1.0, 1.0),
         children: <Widget>[
           CircleAvatar(
-            radius: 36.0,
+            radius: 32.0,
             backgroundColor: const Color(0xff25282B),
             backgroundImage: AssetImage(imagePath),
           ),
           Container(
-            width: 26,
-            height: 26,
+            width: 24,
+            height: 24,
             decoration: const BoxDecoration(
               shape: BoxShape.circle,
               color: Colors.white,
             ),
             child: const Icon(
               Icons.add_a_photo_rounded,
-              size: 18.0,
+              size: 16.0,
               color: Colors.black,
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Profile Icon Example'),
-        ),
-        body: ProfileIcon(
-            imagePath: 'images/Bung-1.png'), // Provide the image path here
       ),
     );
   }
